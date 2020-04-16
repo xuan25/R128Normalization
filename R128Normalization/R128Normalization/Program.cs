9738897934bf2de06489d1c1742daf8ae8ca7557
瑄 <shanboxuan@me.com>
2020-04-16 23:29:24 +0800

    Interactive window

--- R128Normalization/R128Normalization/Program.cs
+++ R128Normalization/R128Normalization/Program.cs
@@ -29,30 +29,71 @@ namespace R128Normalization
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(@"R128Normalization  Copyright (C) 2020  Xuan525");
-            Console.WriteLine(@"This program is under GPLv3 license;");
-            Console.WriteLine(@"This program comes with ABSOLUTELY NO WARRANTY;");
-            Console.WriteLine(@"This is free software, and you are welcome to redistribute it");
-            Console.WriteLine(@"under certain conditions;");
+            Console.WriteLine("R128Normalization  Copyright (C) 2020  Xuan525");
+            Console.WriteLine("This program is under GPLv3 license;");
+            Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.");
+            Console.WriteLine("This is free software, and you are welcome to redistribute it");
+            Console.WriteLine("under certain conditions; type `show c' for details.");
+            Console.WriteLine();
+            Console.WriteLine("Type 'exit' or 'quit' to exit.");
+            Console.WriteLine();
 
-            Directory.CreateDirectory("inputs");
-            Directory.CreateDirectory("outputs");
-
-            DirectoryInfo inputsDirectory = new DirectoryInfo("inputs");
-            FileInfo[] inputs = inputsDirectory.GetFiles();
-            for (int i = 0; i < inputs.Length; i++)
+            while (true)
             {
-                Console.WriteLine("{0}/{1}", i + 1, inputs.Length);
-                string input = inputs[i].FullName;
-                string output = Path.Combine(new DirectoryInfo("outputs").FullName, string.Format("{0}.wav", Path.GetFileNameWithoutExtension(input)));
-
-                // Process
-                Normalization.Normalize(input, output);
-
+                Console.Write("File path / Directory path or Command : ");
+                string input = string.Empty;
+                while (string.IsNullOrEmpty(input))
+                {
+                    input = Console.ReadLine().Trim().Trim('"');
+                }
                 Console.WriteLine();
-                //break;
+
+                if (Directory.Exists(input))
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(input);
+                    string outputDirectory = Path.Combine(directoryInfo.Parent.FullName, $"{directoryInfo.Name}_norm");
+                    Directory.CreateDirectory(outputDirectory);
+
+                    FileInfo[] fileInfos = directoryInfo.GetFiles();
+                    for (int i = 0; i < fileInfos.Length; i++)
+                    {
+                        Console.WriteLine("{0}/{1}", i + 1, fileInfos.Length);
+                        FileInfo fileInfo = fileInfos[i];
+                        Normalization.Normalize(fileInfo.FullName, Path.Combine(outputDirectory, $"{Path.GetFileNameWithoutExtension(fileInfo.Name)}.wav"));
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("Finnished!");
+                }
+                else if (File.Exists(input))
+                {
+                    Normalization.Normalize(input, Path.Combine(Path.GetDirectoryName(input), $"{Path.GetFileNameWithoutExtension(input)}_norm.wav"));
+                    Console.WriteLine();
+                    Console.WriteLine("Finnished!");
+                }
+                else if (input == "show w")
+                {
+                    Console.WriteLine("This program is distributed in the hope that it will be useful,");
+                    Console.WriteLine("but WITHOUT ANY WARRANTY; without even the implied warranty of");
+                    Console.WriteLine("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the");
+                    Console.WriteLine("GNU General Public License for more details.");
+                }
+                else if (input == "show c")
+                {
+                    Console.WriteLine("This program is free software: you can redistribute it and/or modify");
+                    Console.WriteLine("it under the terms of the GNU General Public License as published by");
+                    Console.WriteLine("the Free Software Foundation, either version 3 of the License, or");
+                    Console.WriteLine("(at your option) any later version.");
+                }
+                else if (input == "exit" || input == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Unknow command");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine("Finished");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,26 +1,51 @@
 # R128Normalization
 
-A auto normalization program followed EBU R128 standard
+***中文说明请往下看***
+
+A auto normalization program which followed EBU R128 standard
 
 ## Features
 
 - Auto normalization for sound files
 - Following EBU R128 standard and ITU BS.1770 standard (LUFS)
 - Core algorithm is fully implemented in C#
-- Using [FFmpeg](https://www.ffmpeg.org/) for decoding (allow many input formats) 
+- Using [FFmpeg](https://www.ffmpeg.org/) for decoding files which are not wav format
+- Support batch processing
 
 ## Usage
 
-Place the files like this:
+1. Place the [ffmpeg.exe](https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.2.2-win64-static.zip) with the main program in the same directory or install the FFmpeg in the environment.
+2. Run the main program.
+3. Input or drag the file or the directory into the interactive window and press Enter. 
+4. Wait for processing.
+5. The output will be in the same parent directory with a "_norm" postfix in the filename or directory name.
 
-- root
-  - inputs
-    - file1.mp3
-    - file2.wav
-    - ...
-  - outputs
-    - ...
-  - ffmpeg.exe
-  - R128Normalization.exe
+---
 
-And run
+---
+
+# R128标准化
+
+***For english description, please scroll up***
+
+遵循EBU R128标准的自动标准化程序
+
+## 特性
+
+- 自动标准化声音文件
+- 遵循EBU R128标准和ITU BS.1770标准（LUFS）
+- 核心算法在C＃中完全实现
+- 使用[FFmpeg](https://www.ffmpeg.org/)解码非wav格式的文件
+- 支持批量处理
+
+## 用法
+
+1. 将[ffmpeg.exe](https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.2.2-win64-static.zip)与主程序放在同一目录中，或在环境中安装FFmpeg。
+2. 运行主程序。
+3. 将文件或目录输入或拖动到交互式窗口中，然后按Enter。
+4. 等待处理。
+5. 输出将在相同的父目录中，文件名或目录名称中带有“ _norm”后缀。
+
+---
+
+---
